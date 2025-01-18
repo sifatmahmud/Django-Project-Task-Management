f24d4f2c3fd4dea23af3a1302ddbb068b6067825
Sifat Mahmud <sifatmahmud5293@gmail.com>
2025-01-18 16:47:48 +0600

    dynamic urls implemented

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import show_task
+from .views import show_task, show_specific_task
 
 
 urlpatterns = [
-    path('show-task/', show_task)
+    path('show_task/', show_task)
+    path('show_task/<int:id>/', show_specific_task)
 ]
