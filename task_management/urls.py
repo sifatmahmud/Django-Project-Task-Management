7de6dac42ebd2049b9562539176ad7a3c33c85c8
Sifat Mahmud <sifatmahmud5293@gmail.com>
2025-02-17 01:40:54 +0600

    Home page manager dashboard setup

--- task_management/urls.py
+++ task_management/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from tasks.views import manager_dashboard
 
 
 urlpatterns = [
+    path("", manager_dashboard, name="home"),
     path('admin/', admin.site.urls),
     path("tasks/", include("tasks.urls"))
 ]+ debug_toolbar_urls()
