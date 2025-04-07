1ede48c3237d5f2fc5fb459a36669123f09b4839
Sifat Mahmud <sifatmahmud5293@gmail.com>
2025-04-07 15:46:56 +0600

    10.7 logout

--- users/views.py
+++ users/views.py
@@ -1,42 +1,37 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
-from users.forms import RegisterForm, CustomRegistrationForm
-
+from users.forms import CustomRegistrationForm
 
 # Create your views here.
+
+
 def sign_up(request):
-    if request.method == 'GET':
-        form = CustomRegistrationForm()
+    form = CustomRegistrationForm()
     if request.method == 'POST':
         form = CustomRegistrationForm(request.POST)
         if form.is_valid():
-            # username = form.cleaned_data.get('username')
-            # password = form.cleaned_data.get('password1')
-            # confirm_password = form.cleaned_data.get('password2')
-
-            # if password == confirm_password:
-            #     User.objects.create(username=username, password=password)
-            # else:
-            #     print("Password are not same")
             form.save()
-
-    return render(request, 'registration/register.html',{'form':form})
-
+        else:
+            print("Form is not valid")
+    return render(request, 'registration/register.html', {"form": form})
 
 
 def sign_in(request):
     if request.method == 'POST':
-        username = print(request.POST.get('username'))
-        password = print(request.POST.get('password'))
-
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         print("Doc", username, password)
-
-        user = authenticate(request, username = username, password = password)
-
+        user = authenticate(request, username=username, password=password)
         print(user)
         if user is not None:
             login(request, user)
             return redirect('home')
     return render(request, 'registration/login.html')
+
+
+def sign_out(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('sign-in')
