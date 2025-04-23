957f3d444b8aa0fc53c3ebd77df85b2f2fab813a
Sifat Mahmud <sifatmahmud5293@gmail.com>
2025-04-23 08:59:25 +0600

    12.3 Assign Role to User

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users.views import sign_up, sign_in, sign_out, activate_user, admin_dashboard
+from users.views import sign_up, sign_in, sign_out, activate_user, admin_dashboard, assign_role
 
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('sign-in/', sign_in, name='sign-in'),
     path('sign-out/', sign_out, name='logout'),
     path('activate/<int:user_id>/<str:token>/', activate_user),
-    path('admin/dashboard/', admin_dashboard, name='admin_dashboard')
+    path('admin/dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('admin/<int:user_id>/assign-role/', assign_role, name='assign-role')
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
-from users.forms import CustomRegistrationForm
+from users.forms import CustomRegistrationForm, AssignRoleForm
 from django.contrib import messages
 from users.forms import LoginForm
 from django.contrib.auth.tokens import default_token_generator
@@ -62,8 +62,25 @@ def activate_user(request, user_id, token):
 
 
 def admin_dashboard(request):
-    return render(request, 'admin/dashboard.html')
+    users = User.objects.all()
+    return render(request, 'admin/dashboard.html', {'users':users})
 
+
+
+def assign_role(request, user_id):
+    user = User.objects.get(id=user_id)
+    form = AssignRoleForm()
+
+    if request.method == 'POST':
+        form = AssignRoleForm(request.POST)
+        if form.is_valid():
+            role = form.cleaned_data.get('role')
+            user.groups.clear() # Remove old roles
+            user.groups.add(role)
+            messages.success(request, f"{user.username} has been assigned to the {role.name} role")
+            return redirect('admin_dashboard')
+    
+    return render(request, 'admin/assigned_role.html', {"form":form})
 
 """
 Admin
